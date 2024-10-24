d247327d116c112b1c232caab0d3a28eebfa1c59
Paul Wheeler <paul@free-side.net>
2024-10-23 23:13:17 -1000

    Added a default gatecheck configuration and added merge capabilities
    Previously if an explicit gatecheck configuration was not provided the
    default behavior was essentially to validate nothing. Now there is a
    reasonable default that can be overriden by user specified config.

--- pkg/pipelines/deploy.go
+++ pkg/pipelines/deploy.go
@@ -1,10 +1,14 @@
 package pipelines
 
 import (
+	_ "embed"
 	"errors"
 	"fmt"
+	"github.com/jarxorg/tree"
+	"gopkg.in/yaml.v3"
 	"io"
 	"log/slog"
+	"os"
 	"path"
 	"portage/pkg/shell"
 )
@@ -37,6 +41,13 @@ func (p *Deploy) preRun() error {
 	return nil
 }
 
+//go:embed gatecheck.defaults.yml
+var gatecheckDefaultConfig string
+
+func mkDeploymentError(cause error) error {
+	return fmt.Errorf("deployment Validation failed: %w", cause)
+}
+
 func (p *Deploy) Run() error {
 	if !p.config.Deploy.Enabled {
 		slog.Warn("deployment pipeline disabled, skip.")
@@ -48,15 +59,66 @@ func (p *Deploy) Run() error {
 
 	slog.Warn("deployment pipeline is a beta feature. Only gatecheck validation will be conducted.")
 
-	err := shell.GatecheckValidate(
+	gatecheckConfigPath := path.Join(p.config.ArtifactDir, "gatecheck-config.yml")
+	gatecheckConfig, err := os.OpenFile(gatecheckConfigPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	defer gatecheckConfig.Close()
+
+	if err != nil {
+		return mkDeploymentError(err)
+	}
+
+	if p.config.Deploy.GatecheckConfigFilename != "" {
+		customConfigFile, err := os.ReadFile(p.config.Deploy.GatecheckConfigFilename)
+		if err != nil {
+			return mkDeploymentError(err)
+		}
+
+		// Unmarshal the YAML into a map
+		var customConfig tree.Map
+		err = yaml.Unmarshal(customConfigFile, &customConfig)
+		if err != nil {
+			return mkDeploymentError(err)
+		}
+
+		var baseConfig tree.Map
+		err = yaml.Unmarshal([]byte(gatecheckDefaultConfig), &baseConfig)
+		if err != nil {
+			return mkDeploymentError(err)
+		}
+
+		// Merge the trees and write the result to gatecheckConfig os.File
+		mergedConfig := tree.Merge(baseConfig, customConfig, tree.MergeOptionReplaceArray|tree.MergeOptionOverrideMap)
+
+		b, err := yaml.Marshal(mergedConfig)
+		if err != nil {
+			return mkDeploymentError(err)
+		}
+		_, err = gatecheckConfig.Write(b)
+		if err != nil {
+			return mkDeploymentError(err)
+		}
+	} else {
+		_, err = gatecheckConfig.Write([]byte(gatecheckDefaultConfig))
+		if err != nil {
+			return mkDeploymentError(err)
+		}
+		gatecheckConfig.Close()
+	}
+
+	err = AddBundleFile(p.DryRunEnabled, p.runtime.bundleFilename, gatecheckConfigPath, p.Stderr)
+	if err != nil {
+		return mkDeploymentError(err)
+	}
+
+	err = shell.GatecheckValidate(
 		shell.WithDryRun(p.DryRunEnabled),
 		shell.WithStderr(p.Stderr),
 		shell.WithStdout(p.Stdout),
 		shell.WithTargetFile(p.runtime.bundleFilename),
-		shell.WithConfigFile(p.config.Deploy.GatecheckConfigFilename),
+		shell.WithConfigFile(gatecheckConfigPath),
 	)
 	if err != nil {
-		return fmt.Errorf("Deployment Validation failed: %w", err)
+		return mkDeploymentError(err)
 	}
 
 	return nil
